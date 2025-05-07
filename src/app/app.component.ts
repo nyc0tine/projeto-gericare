5e3537856315b5a60250303682b928febd55d003
Micaele Albuquerque <marli24moura@gmail.com>
2025-05-07 11:18:42 -0300

    Adição do componente header
    atualizações no app.component.ts e no app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component'; // Importe o HeaderComponent
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent], // Adicione o HeaderComponent aqui
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
