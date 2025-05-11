d26688044633f3b185b421727d50853f6a9d1d26
Maria Cristina <mariacristinan147@gmail.com>
2025-05-10 22:21:21 -0300

    commit
    Foi modificado os componentes do header, além de baixado o @angular/material

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  menuAberto = false;
 }
