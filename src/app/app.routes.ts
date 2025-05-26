f815b8c77ed3d03dbc8308d1e7cae4faceb4c1c2
Nycole <nycolelacerda@gmail.com>
2025-05-26 20:45:12 -0300

    tela inicial feita
    gostaria apenas de ajeitar pra centralizar a timeline e as bolinhas de check, pois eles tao tipo direita esquerda, como é uma tela pequena, nao influencia muito, mas numa tela grande fica paia. ainda vou adicionar as funcionalidades so ta visual

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component'; // Importe o componente
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: PaginaInicialComponent, pathMatch: 'full' }
+  
+];
